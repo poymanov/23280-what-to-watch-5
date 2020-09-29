ffd8dcf1bcf7990a820e10567ba1d8033f69392d
poymanov <n.poymanov@gmail.com>
2020-09-29 23:29:32 +0300

    Добавлена главная страница

---
+++ src/components/main/main.jsx
@@ -0,0 +1,140 @@
+import React, {Fragment} from "react";
+import PropTypes from "prop-types";
+
+const Main = (props) => {
+  const {promoMovie, genres, movies} = props;
+
+  return (
+    <Fragment>
+      <section className="movie-card">
+        <div className="movie-card__bg">
+          <img src={promoMovie.preview} alt={promoMovie.title} />
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <header className="page-header movie-card__head">
+          <div className="logo">
+            <a className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+            </div>
+          </div>
+        </header>
+
+        <div className="movie-card__wrap">
+          <div className="movie-card__info">
+            <div className="movie-card__poster">
+              <img src={promoMovie.poster} alt={promoMovie.title + ` poster`} width="218" height="327"/>
+            </div>
+
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{promoMovie.title}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{promoMovie.genre}</span>
+                <span className="movie-card__year">{promoMovie.year}</span>
+              </p>
+
+              <div className="movie-card__buttons">
+                <button className="btn btn--play movie-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use href="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list movie-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use href="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+          <ul className="catalog__genres-list">
+            <li className="catalog__genres-item catalog__genres-item--active">
+              <a href="#" className="catalog__genres-link">All genres</a>
+            </li>
+            {genres.map((genre) => {
+              return (<li key={genre.id} className="catalog__genres-item">
+                <a href="#" className="catalog__genres-link">{genre.title}</a>
+              </li>);
+            })}
+          </ul>
+
+          <div className="catalog__movies-list">
+            {movies.map((movie) => {
+              return (
+                <article key={movie.id} className="small-movie-card catalog__movies-card">
+                  <div className="small-movie-card__image">
+                    <img src={movie.image} alt="{movie.title}" width="280" height="175"/>
+                  </div>
+                  <h3 className="small-movie-card__title">
+                    <a className="small-movie-card__link" href={movie.image}>{movie.title}</a>
+                  </h3>
+                </article>
+              );
+            })}
+          </div>
+
+          <div className="catalog__more">
+            <button className="catalog__button" type="button">Show more</button>
+          </div>
+        </section>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </Fragment>
+  );
+};
+
+Main.propTypes = {
+  promoMovie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    poster: PropTypes.string.isRequired,
+    preview: PropTypes.string.isRequired
+  }).isRequired,
+  genres: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired
+      })
+  ).isRequired,
+  movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired
+      })
+  ).isRequired
+};
+
+export default Main;
