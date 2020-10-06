854c9736333ac441f32ea5a7b5cfb9a4f17d99ba
poymanov <n.poymanov@gmail.com>
2020-10-06 18:33:10 +0300

    Рефакторинг отрисовки ссылок в шапке проекта

--- src/components/film/film.jsx
+++ src/components/film/film.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment} from "react";
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
 
 const Film = (props) => {
   const {relatedMovies} = props;
@@ -15,11 +16,11 @@ const Film = (props) => {
 
           <header className="page-header movie-card__head">
             <div className="logo">
-              <a href="/" className="logo__link">
+              <Link to='/' className="logo__link">
                 <span className="logo__letter logo__letter--1">W</span>
                 <span className="logo__letter logo__letter--2">T</span>
                 <span className="logo__letter logo__letter--3">W</span>
-              </a>
+              </Link>
             </div>
 
             <div className="user-block">
--- src/components/my-list/my-list.jsx
+++ src/components/my-list/my-list.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
 
 const MyList = (props) => {
   const {userMovies} = props;
@@ -7,11 +8,11 @@ const MyList = (props) => {
     <div className="user-page">
       <header className="page-header user-page__head">
         <div className="logo">
-          <a href="/" className="logo__link">
+          <Link to='/' className="logo__link">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          </Link>
         </div>
 
         <h1 className="page-title user-page__title">My list</h1>
